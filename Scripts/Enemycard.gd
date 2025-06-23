f906d2708446697ddace9ce9d48c3f1257a0ad2a
Mertijis <victorfinetosilva@uol.com.br>
2025-06-22 23:33:09 -0300

    adição som e efeito de carta não podendo atacar

--- Scripts/Enemycard.gd
+++ Scripts/Enemycard.gd
@@ -6,3 +6,24 @@ var poder_original
 var vida
 var card_slot_card_in
 var anime
+
+var is_exhausted = false
+var original_rotation = 0.0
+const EXHAUSTED_ROTATION = 0.35
+
+func set_exhausted(state):
+	is_exhausted = state
+	if state:
+		# Aplicar rotação permanente
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "rotation", EXHAUSTED_ROTATION, 0.2)
+	else:
+		# Remover rotação
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "rotation", original_rotation, 0.2)
+		
+func reset_visuals():
+	is_exhausted = false
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "rotation", original_rotation, 0.1)
+	tween.parallel().tween_property(self, "modulate", Color(1, 1, 1, 1), 0.1)
